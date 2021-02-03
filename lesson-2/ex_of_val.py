4d465dd7882e7632372c02f0f8a2416c146d57cb
Winzenz <mva.prog@gmail.com>
2021-02-04 05:35:41 +0600

    home work lesson 1

--- lesson-2/ex_of_val.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Введите кол-во элементов списка "))
-list = []
-for i in range(n):
-    list.append(input("Введите элемент "))
-for i in range(1, len(list), 2):
-    list[i - 1], list[i] = list[i], list[i - 1]
-list_2 = ' '.join([str(i) for i in list])
-print("Список с замененными занчиниями ",list_2)
